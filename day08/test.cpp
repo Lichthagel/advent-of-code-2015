911dd7db624bb9231bc8ce73471490f15dea9028
Jens Gatzweiler <lichthagel@tuta.io>
2023-11-13 09:49:16 +0000

    fix: day08 test

--- day08/test.cpp
+++ day08/test.cpp
@@ -5,43 +5,67 @@
 #ifdef PART1
 TEST(Day08_Part1, Example1)
 {
-  ASSERT_EQ(run("\"\""), std::make_pair(2ull, 0ull));
+  std::pair<size_t, size_t> result = run("\"\"");
+
+  ASSERT_EQ(result.first, 2ull);
+  ASSERT_EQ(result.second, 0ull);
 }
 
 TEST(Day08_Part1, Example2)
 {
-  ASSERT_EQ(run("\"abc\""), std::make_pair(5ull, 3ull));
+  std::pair<size_t, size_t> result = run("\"abc\"");
+
+  ASSERT_EQ(result.first, 5ull);
+  ASSERT_EQ(result.second, 3ull);
 }
 
 TEST(Day08_Part1, Example3)
 {
-  ASSERT_EQ(run("\"aaa\\\"aaa\""), std::make_pair(10ull, 7ull));
+  std::pair<size_t, size_t> result = run("\"aaa\\\"aaa\"");
+
+  ASSERT_EQ(result.first, 10ull);
+  ASSERT_EQ(result.second, 7ull);
 }
 
 TEST(Day08_Part1, Example4)
 {
-  ASSERT_EQ(run("\"\\x27\""), std::make_pair(6ull, 1ull));
+  std::pair<size_t, size_t> result = run("\"\\x27\"");
+
+  ASSERT_EQ(result.first, 6ull);
+  ASSERT_EQ(result.second, 1ull);
 }
 #endif
 
 #ifdef PART2
 TEST(Day08_Part2, Example1)
 {
-  ASSERT_EQ(run("\"\""), std::make_pair(2ull, 6ull));
+  std::pair<size_t, size_t> result = run("\"\"");
+
+  ASSERT_EQ(result.first, 2ull);
+  ASSERT_EQ(result.second, 6ull);
 }
 
 TEST(Day08_Part2, Example2)
 {
-  ASSERT_EQ(run("\"abc\""), std::make_pair(5ull, 9ull));
+  std::pair<size_t, size_t> result = run("\"abc\"");
+
+  ASSERT_EQ(result.first, 5ull);
+  ASSERT_EQ(result.second, 9ull);
 }
 
 TEST(Day08_Part2, Example3)
 {
-  ASSERT_EQ(run("\"aaa\\\"aaa\""), std::make_pair(10ull, 16ull));
+  std::pair<size_t, size_t> result = run("\"aaa\\\"aaa\"");
+
+  ASSERT_EQ(result.first, 10ull);
+  ASSERT_EQ(result.second, 16ull);
 }
 
 TEST(Day08_Part2, Example4)
 {
-  ASSERT_EQ(run("\"\\x27\""), std::make_pair(6ull, 11ull));
+  std::pair<size_t, size_t> result = run("\"\\x27\"");
+
+  ASSERT_EQ(result.first, 6ull);
+  ASSERT_EQ(result.second, 11ull);
 }
 #endif
