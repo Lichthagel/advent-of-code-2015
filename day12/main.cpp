7ab46b5e4420ad9087bf6f449eb3024b439354cd
Lichthagel <lichthagel@tuta.io>
2023-11-15 16:04:31 +0100

    day 12 part 2

--- day12/main.cpp
+++ day12/main.cpp
@@ -1,35 +1,141 @@
 #include <string>
 #include <iostream>
+#ifdef PART2
+#include <stack>
+#endif
+#include <cassert>
 #include <input.hpp>
 
 int run(std::string input)
 {
+
+#ifdef PART1
   int sum = 0;
+#endif
+#ifdef PART2
+  std::stack<std::pair<int, bool>> stack;
+
+  // first: sum, second: is_array
+  std::pair<int, bool> current;
+#endif
+
+  size_t end;
+  int number;
 
   for (size_t pos = 0; pos < input.size();)
   {
-    pos = input.find_first_of("-0123456789", pos);
-
-    if (pos == std::string::npos)
+    switch (input[pos])
     {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '-':
+      end = input.find_first_not_of("0123456789", pos + 1);
+
+      assert(end != std::string::npos);
+
+      number = std::stoi(input.substr(pos, end - pos));
+
+#ifdef PART1
+      sum += number;
+#endif
+#ifdef PART2
+      assert(!stack.empty());
+
+      stack.top().first += number;
+#endif
+      pos = end;
+      break;
+
+#ifdef PART2
+    case '{':
+      stack.push(std::make_pair(0, false));
+
+      pos++;
+      break;
+    case '[':
+      stack.push(std::make_pair(0, true));
+
+      pos++;
+      break;
+
+    case '}':
+    case ']':
+      assert(stack.size() > 0);
+      assert(stack.top().second == (input[pos] == ']'));
+
+      current = stack.top();
+      stack.pop();
+
+      if (stack.size() == 0)
+      {
+        return current.first;
+      }
+      else
+      {
+        stack.top().first += current.first;
+      }
+
+      pos++;
+      break;
+
+    case 'r':
+      // if red and current item is an object, truncate current object
+      if (input.substr(pos, 3) == "red" && stack.size() > 0 && !stack.top().second)
+      {
+        stack.pop();
+
+        // skip current object
+        unsigned int object_depth = 1;
+        while (object_depth > 0)
+        {
+          pos = input.find_first_of("{}", pos);
+          assert(pos != std::string::npos);
+
+          if (input[pos] == '{')
+          {
+            object_depth++;
+          }
+          else
+          {
+            object_depth--;
+          }
+          pos++;
+        }
+
+        if (stack.size() == 0)
+        {
+          return 0;
+        }
+      }
+      else
+      {
+        pos++;
+      }
+
+      break;
+#endif
+
+    default:
+      pos++;
       break;
     }
-
-    size_t end = input.find_first_not_of("0123456789", pos + 1);
-
-    if (end == std::string::npos)
-    {
-      end = input.size();
-    }
-
-    int number = std::stoi(input.substr(pos, end - pos));
-
-    sum += number;
-
-    pos = end;
   }
 
+#ifdef PART1
   return sum;
+#endif
+#ifdef PART2
+  assert(stack.size() == 0);
+  assert(false);
+#endif
 }
 
 #ifndef TEST
--- day12/test.cpp
+++ day12/test.cpp
@@ -27,3 +27,40 @@ TEST(Day12_Part1, Example4)
   EXPECT_EQ(run("{}"), 0);
 }
 #endif
+
+#ifdef PART2
+TEST(Day12_Part2, Example1)
+{
+  EXPECT_EQ(run("[1,2,3]"), 6);
+}
+
+TEST(Day12_Part2, Example2)
+{
+  EXPECT_EQ(run("[1,{\"c\":\"red\",\"b\":2},3]"), 4);
+}
+
+TEST(Day12_Part2, Example3)
+{
+  EXPECT_EQ(run("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}"), 0);
+}
+
+TEST(Day12_Part2, Example4)
+{
+  EXPECT_EQ(run("[1,\"red\",5]"), 6);
+}
+
+TEST(Day12_Part2, Test1)
+{
+  EXPECT_EQ(run("[93,\"red\",183,\"blue\",\"red\",30,-16,\"red\",\"red\",\"yellow\"]"), 290);
+}
+
+TEST(Day12_Part2, Test2)
+{
+  EXPECT_EQ(run("[\"violet\",19,\"orange\",\"yellow\",\"red\",\"violet\",\"blue\",\"violet\",{\"e\":\"yellow\",\"a\":\"violet\",\"d\":137,\"c\":\"blue\",\"h\":197,\"b\":\"orange\",\"g\":{\"e\":\"yellow\",\"c\":\"orange\",\"a\":18,\"b\":42,\"d\":183,\"f\":142},\"f\":68}]"), 806);
+}
+
+TEST(Day12_Part2, Test3)
+{
+  EXPECT_EQ(run("{\"d\":[\"red\",4,6]}"), 10);
+}
+#endif
