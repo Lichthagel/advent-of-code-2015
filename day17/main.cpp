a90315a11e0732350f575e76e0086ae0acd8a3ec
Lichthagel <hey@lichthagel.de>
2023-12-08 00:01:43 +0100

    day17 part 2

--- day17/main.cpp
+++ day17/main.cpp
@@ -2,22 +2,42 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#ifdef PART2
+#include <unordered_map>
+#endif
 
 #define EGGNOG_LITERS 150
 
-int combinations(std::vector<unsigned int> bin_sizes, bool *use_bin, size_t index, unsigned int liters_left, unsigned int total_liters = EGGNOG_LITERS)
+int combinations(
+    std::vector<unsigned int> &bin_sizes,
+#ifdef PART2
+    std::unordered_map<unsigned int, unsigned int> &combinations_map,
+#endif
+    bool *use_bin,
+    size_t index,
+    unsigned int liters_left,
+    unsigned int total_liters)
 {
   if (index == bin_sizes.size())
   {
     unsigned int liters = 0;
+#ifdef PART2
+    unsigned int nused = 0;
+#endif
 
     for (size_t i = 0; i < bin_sizes.size(); i++)
     {
       liters += bin_sizes[i] * use_bin[i];
+#ifdef PART2
+      nused += use_bin[i];
+#endif
     }
 
     if (liters == total_liters)
     {
+#ifdef PART2
+      combinations_map[nused]++;
+#endif
       return 1;
     }
     else
@@ -25,14 +45,30 @@ int combinations(std::vector<unsigned int> bin_sizes, bool *use_bin, size_t inde
       return 0;
     }
   }
-  
+
   use_bin[index] = false;
-  int total = combinations(bin_sizes, use_bin, index + 1, liters_left, total_liters);
+  int total = combinations(
+      bin_sizes,
+#ifdef PART2
+      combinations_map,
+#endif
+      use_bin,
+      index + 1,
+      liters_left,
+      total_liters);
 
   if (bin_sizes[index] <= liters_left)
   {
     use_bin[index] = true;
-    total += combinations(bin_sizes, use_bin, index + 1, liters_left - bin_sizes[index], total_liters);
+    total += combinations(
+      bin_sizes,
+#ifdef PART2
+      combinations_map,
+#endif
+      use_bin,
+      index + 1,
+      liters_left - bin_sizes[index],
+      total_liters);
   }
 
   return total;
@@ -49,12 +85,30 @@ int main(int argc, char const *argv[])
   {
     bin_sizes.push_back(std::stoi(line));
   }
-
-  // std::vector<unsigned int> bin_sizes = {20, 15, 10, 5, 5};
-
+  
   bool *use_bin = new bool[bin_sizes.size()];
 
-  std::cout << combinations(bin_sizes, use_bin, 0, EGGNOG_LITERS) << std::endl;
+#ifdef PART1
+  std::cout << combinations(bin_sizes, use_bin, 0, EGGNOG_LITERS, EGGNOG_LITERS) << std::endl;
+#endif
+
+#ifdef PART2
+  std::unordered_map<unsigned int, unsigned int> combinations_map;
+
+  combinations(bin_sizes, combinations_map, use_bin, 0, EGGNOG_LITERS, EGGNOG_LITERS);
+
+  unsigned int min = UINT32_MAX;
+
+  for (auto combination : combinations_map)
+  {
+    if (combination.first < min)
+    {
+      min = combination.first;
+    }
+  }
+
+  std::cout << combinations_map[min] << std::endl;
+#endif
 
   delete[] use_bin;
 
