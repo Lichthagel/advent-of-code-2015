b4000872d7bf2c53a2ecf601cbb8580155bec2ed
Lichthagel <hey@lichthagel.de>
2023-12-07 20:53:38 +0100

    day14: part2

--- day14/main.cpp
+++ day14/main.cpp
@@ -52,23 +52,44 @@ unsigned int Reindeer::getDistance(unsigned int time)
   return distance;
 }
 
-#ifndef TEST
-int main(int argc, char const *argv[])
+class Race
 {
-  Input input(__FILE__);
-
+private:
   std::vector<Reindeer> reindeers;
 
+public:
+  Race(Input input);
+  Race(std::vector<Reindeer> reindeers);
+  ~Race();
+  unsigned int totalDistanceWinner(unsigned int time);
+  std::vector<unsigned int> points(unsigned int time);
+  unsigned int pointsWinner(unsigned int time);
+};
+
+Race::Race(Input input)
+{
   for (auto line : input)
   {
-    reindeers.push_back(Reindeer(line));
+    this->reindeers.push_back(Reindeer(line));
   }
+}
 
+Race::Race(std::vector<Reindeer> reindeers)
+{
+  this->reindeers = reindeers;
+}
+
+Race::~Race()
+{
+}
+
+unsigned int Race::totalDistanceWinner(unsigned int time)
+{
   unsigned int max = 0;
 
   for (auto reindeer : reindeers)
   {
-    unsigned int distance = reindeer.getDistance(2503);
+    unsigned int distance = reindeer.getDistance(time);
 
     if (distance > max)
     {
@@ -76,9 +97,80 @@ int main(int argc, char const *argv[])
     }
   }
 
-  std::cout << max << std::endl;
+  return max;
+}
 
-  /* code */
+std::vector<unsigned int> Race::points(unsigned int time)
+{
+  size_t n = this->reindeers.size();
+
+  std::vector<unsigned int> points(this->reindeers.size());
+  std::vector<unsigned int> distances(this->reindeers.size());
+  std::vector<int> power(this->reindeers.size());
+
+  for (size_t i = 0; i < n; i++)
+  {
+    points[i] = 0;
+    distances[i] = 0;
+    power[i] = this->reindeers[i].flyTime;
+  }
+
+  for (unsigned int t = 0; t < time; t++)
+  {
+    unsigned int maxDistance = 0;
+
+    for (size_t i = 0; i < n; i++)
+    {
+
+      if (power[i] <= -((int)this->reindeers[i].restTime))
+      {
+        power[i] = this->reindeers[i].flyTime;
+      }
+
+      if (power[i] > 0)
+      {
+        distances[i] += this->reindeers[i].flySpeed;
+      }
+
+      power[i]--;
+      if (distances[i] >= maxDistance)
+      {
+        maxDistance = distances[i];
+      }
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+      if (distances[i] == maxDistance)
+      {
+        points[i]++;
+      }
+    }
+  }
+
+  return points;
+}
+
+unsigned int Race::pointsWinner(unsigned int time)
+{
+  std::vector<unsigned int> points = this->points(time);
+
+  return *std::max_element(points.begin(), points.end());
+}
+
+#ifndef TEST
+int main(int argc, char const *argv[])
+{
+  Race race(Input(__FILE__));
+
+#ifdef PART1
+  std::cout << race.totalDistanceWinner(2503) << std::endl;
+#endif
+
+#ifdef PART2
+  std::cout << race.pointsWinner(2503) << std::endl;
+#endif
+
   return 0;
 }
 #endif
--- day14/test.cpp
+++ day14/test.cpp
@@ -3,63 +3,44 @@
 #include "main.cpp"
 
 #ifdef PART1
-TEST(Day14_Part1, Example1) {
+TEST(Day14_Part1, Example_Comet)
+{
   Reindeer comet("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.");
 
   EXPECT_EQ(comet.getDistance(1), 14);
-}
-
-TEST(Day14_Part1, Example2) {
-  Reindeer comet("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.");
-
   EXPECT_EQ(comet.getDistance(10), 140);
-}
-
-TEST(Day14_Part1, Example3) {
-  Reindeer comet("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.");
-
   EXPECT_EQ(comet.getDistance(11), 140);
-}
-
-TEST(Day14_Part1, Example4) {
-  Reindeer comet("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.");
-
   EXPECT_EQ(comet.getDistance(12), 140);
-}
-
-TEST(Day14_Part1, Example5) {
-  Reindeer comet("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.");
-
   EXPECT_EQ(comet.getDistance(1000), 1120);
 }
 
-TEST(Day14_Part1, Example6) {
+TEST(Day14_Part1, Example_Dancer)
+{
   Reindeer dancer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.");
 
   EXPECT_EQ(dancer.getDistance(1), 16);
-}
-
-TEST(Day14_Part1, Example7) {
-  Reindeer dancer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.");
-
   EXPECT_EQ(dancer.getDistance(10), 160);
-}
-
-TEST(Day14_Part1, Example8) {
-  Reindeer dancer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.");
-
   EXPECT_EQ(dancer.getDistance(11), 176);
-}
-
-TEST(Day14_Part1, Example9) {
-  Reindeer dancer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.");
-
   EXPECT_EQ(dancer.getDistance(12), 176);
-}
-
-TEST(Day14_Part1, Example10) {
-  Reindeer dancer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.");
-
   EXPECT_EQ(dancer.getDistance(1000), 1056);
+}
+#endif
+
+#ifdef PART2
+TEST(Day14_Part2, Example_Race)
+{
+  std::vector<Reindeer> reindeers = {Reindeer("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."),
+                                     Reindeer("Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.")};
+
+  Race race(reindeers);
+
+  EXPECT_EQ(race.pointsWinner(1), 1);
+  EXPECT_EQ(race.pointsWinner(140), 139);
+  EXPECT_EQ(race.pointsWinner(1000), 689);
+
+  std::vector<unsigned int> points = race.points(1000);
+
+  EXPECT_EQ(points[0], 312);
+  EXPECT_EQ(points[1], 689);
 }
 #endif
